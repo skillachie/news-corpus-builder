29b0ac70e0b1bbeb9b01be77804966600237a2d8
Skillachie <dwaynecampbell13@gmail.com>
2015-10-01 10:33:38 -0400

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='news-corpus-builder',
-    version='0.1.0',
+    version='0.1.3',
     packages=find_packages(),
     include_package_data=True,
     author='Dwayne V Campbell',
